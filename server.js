8fc4f88cfd9e91ffb5c24dbf807982e7f4924773
Deepak-0525 <deepaksaravanan45@gmail.com>
2026-05-11 12:56:56 +0530

    shopify sync backend

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "ngrok-skip-browser-warning"]
+}));
 app.use(express.json());
 
 const SHOP = process.env.SHOPIFY_STORE;
@@ -202,4 +206,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
-});2
+});
